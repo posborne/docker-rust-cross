2238d991993278478aecc8f28f192baa30f2c1d4
Paul Osborne <osbpau@gmail.com>
2016-08-11 01:33:30 -0500

    Changes to support templatized builds to support many versions
    New Format: posborne/rust-cross-${platform}:${rust_version}.  Not fully
    tested as of yet due to rate limiting on downloading tarballs from
    rust lang.
    
    Signed-off-by: Paul Osborne <osbpau@gmail.com>

---
+++ build-all.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+set -eu
+
+TOPDIR=$(dirname "$0")
+BASE_NAME="posborne/rust-cross-"
+
+PLATFORMS=${PLATFORMS:-"\
+  base \
+  x86 \
+  arm \
+  mips \
+  android \
+"}
+
+VERSIONS=${VERSIONS:-"\
+  1.7.0 \
+  1.8.0 \
+  1.9.0 \
+  1.10.0 \
+  beta \
+  stable \
+  nightly \
+"}
+
+build_image() {
+    local platform=$1
+    local rust_version=$2
+    local build_dir="$TOPDIR/build/${platform}:${rust_version}"
+
+    echo "Building posborne/rust-cross-${platform}:${rust_version} ..."
+
+    # prepare the build directory
+    rm -rf ${build_dir}
+    mkdir -p ${build_dir}
+
+    # convert template for the platform
+    cp -r ${TOPDIR}/${platform}/* ${build_dir}/
+    mustache - ${TOPDIR}/${platform}/Dockerfile.${platform}.mustache >${build_dir}/Dockerfile <<EOF
+rust_version: "${rust_version}"
+EOF
+
+    # build the image
+    docker build \
+           -t "${BASE_NAME}${platform}:${rust_version}" \
+           "${build_dir}"
+
+    echo "Done with posborne/rust-cross-${platform} ... Waiting 30s before continuing"
+}
+
+for platform in $PLATFORMS; do
+    for version in $VERSIONS; do
+        build_image ${platform} ${version}
+    done
+done
+
+build_image base 1.7.0
+build_image base 1.8.0
